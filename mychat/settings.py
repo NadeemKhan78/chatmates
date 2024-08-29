f59a1f13aee4b57d594ac9dab96ac38f4b4bd218
god <nknadeemkhan876@gmail.com>
2024-08-29 15:37:04 +0530

    changed database

--- mychat/settings.py
+++ mychat/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'mychat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
-        'NAME': 'chatmatedb',
-        'USER': 'postgres',
-        'PASSWORD': 'nknadeem1978',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
+#         'NAME': 'chatmatedb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'nknadeem1978',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
