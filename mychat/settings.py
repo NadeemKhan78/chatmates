e4c9ab7585001eb6f78675dba27870b92382b5bf
god <nknadeemkhan876@gmail.com>
2024-08-30 21:25:58 +0530

    db

--- mychat/settings.py
+++ mychat/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'mychat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
         'NAME': 'defaultdb',
         'USER': 'avnadmin',
         'PASSWORD': 'AVNS_2ppbvpUJUs9X5-VaPUx',
